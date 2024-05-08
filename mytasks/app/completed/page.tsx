eef4b7e733e70ac77da677df0afcd4af3c90da8d
Munguntsetseg <munguunuu116@gmail.com>
2024-05-08 17:30:35 +0800

    create prisma and fix something

--- mytasks/app/completed/page.tsx
+++ mytasks/app/completed/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { useGlobalState } from "../context/globalProvider";
-import Tasks from "../Tasks/Tasks";
+import Tasks from "../Components/Tasks/Tasks";
 
 function page() {
   const { completedTasks } = useGlobalState();
